9a5321859aa6b38267ba2255f36988a545989a56
J-leg <jamesz96.au@gmail.com>
2020-11-23 12:11:46 +1100

    New Logo and favicon

--- components/splash.tsx
+++ components/splash.tsx
@@ -2,38 +2,38 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import { makeStyles, Theme } from '@material-ui/core/styles';
+import Image from 'next/image'
 
 const useStyles = makeStyles((theme: Theme) => ({
-  paper: {
-  },
-  title: {
-    text: theme.palette.primary
-  },
   button: {
-    color: theme.palette.text.secondary,
+    color: theme.palette.secondary.dark,
   },
 }));
 
 function Splash() {
-	const classes = useStyles()
+  const classes = useStyles()
   return (
     <Grid container direction="column" alignItems="center" justify="center" spacing={2} style={{ minHeight: '100vh' }}>
       <Grid item xs={12}>
-        <Paper elevation={0} className={classes.paper}>
-          <Typography variant="h1" component="h2" gutterBottom>
-            players.fyi
-          </Typography>
-        </Paper>
+        <Image src="/images/jugg.png" width={280} height={390}/>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography color='textPrimary' variant="h1" component="h2" gutterBottom>
+          players.fyi
+        </Typography>
       </Grid>
       <Grid item xs={12}>
         <Grid container spacing={1}>
           <Grid item xs>
-              <Button variant="outlined" href="/stats/lastDays" className={classes.button}>Last 30 Days</Button>
+            <Button variant="outlined" href="/stats/lastDays" className={classes.button}> 
+              <Typography noWrap>Last 30 Days</Typography>
+            </Button>
           </Grid>
           <Grid item xs>
-            <Button variant="outlined" href="/stats/lastDays" className={classes.button}>Last 12 Months</Button>
+            <Button variant="outlined" href="/stats/lastDays" className={classes.button}>
+              <Typography noWrap>Last 12 Months</Typography>
+            </Button>
           </Grid>
         </Grid>
       </Grid>
