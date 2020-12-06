f6c76db77e47a8a6e44cfdd7f5f2022e88893e49
J-leg <jamesz96.au@gmail.com>
2020-12-06 15:43:37 +1100

    New date key builder

--- components/details.tsx
+++ components/details.tsx
@@ -12,11 +12,6 @@ export default function Details({ appData }) {
     <React.Fragment>
       <Typography component="h2" variant="h6" color="textPrimary" gutterBottom>Details</Typography>
       <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-          </TableRow>
-        </TableHead>
         <TableBody>
           {appData.map((row: any) => (
             <TableRow key={row.name}>
