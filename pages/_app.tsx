d28485872cf91b425855c1998300dcc5992dbf9a
J-leg <jamesz96.au@gmail.com>
2020-12-12 20:41:13 +1100

    Replace favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from 'styles/theme';
 
-const title: string = "{ players.fyi }";
+const title: string = "players.fyi";
 
 export default function App({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
@@ -22,8 +22,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/papier.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/papier.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/quelling_blade.svg" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/quelling_blade.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
